50cb74f720985397f4f6623fad19f81078628f46
war823 <97470186+war823@users.noreply.github.com>
2023-07-21 23:49:24 +0200

    Add files via upload

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Max Volokhovskyi on 09/06/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
